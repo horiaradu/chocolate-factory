60a280e15f986acf6d5d55805962476fa8fb547e
kale-io <cos+a@iuvo.ai>
2018-04-05 11:35:48 +0300

    Convert Non-strict to strict equality checking Convert non-strict equality checking, using `==`, to the strict version, using `===`.

--- src/test.js
+++ src/test.js
@@ -1,12 +1,12 @@
-if (1 == 1) {}
-if (2 == 123) {}
-if (3 == 3) {} else {}
-if (4 == 5) {} else {}
+if (1 === 1) {}
+if (2 === 123) {}
+if (3 === 3) {} else {}
+if (4 === 5) {} else {}
 
 function dummt() {
   if (5 === 0) {
   }
-  if (a == b) {
+  if (a === b) {
   }
   try {
     1 + 1;
